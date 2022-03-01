9bb73c461e494d0590bfed3afb262e15b73b8aab
Diana <diana.zhusupbekova.kk@gmail.com>
2022-03-01 17:27:24 +0600

    feat: add files

---
+++ app.js
@@ -0,0 +1,27 @@
+/* Book */
+function Book() {
+
+}
+
+/* UI Constructor */
+function UI() {}
+
+/* UI Prototype Methods */
+UI.prototype.addBookToList = function (book) {
+    // Find the table
+    // Create tr element
+    // Insert cols
+    // Append to table 
+};
+
+/* Clear field */
+UI.prototype.clearFileds = function () {};
+
+/* Event Listeners */
+document.getElementById("book-form").addEventListener("submit", (e) => {
+    // Get form values
+    // Instance of book
+    // Instance of UI
+    // Add book to List
+    // Clear fields
+})
